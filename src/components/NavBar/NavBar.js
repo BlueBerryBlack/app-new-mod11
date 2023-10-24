fc1b664d195a12183df71f99035a72ba6d181dc4
Jagoda Pogroszewsla <pogroszewskadesign@gmail.com>
2023-10-24 15:39:50 +0200

    NavLink

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import styles from './NavBar.module.scss';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
@@ -6,9 +7,12 @@ const NavBar = () => {
             <span className={styles.pictogram + ' fa fa-tasks'} />
             <div className={styles.navigation}>
                 <ul>
-                    <li><a href="/" >Home</a></li>
-                    <li><a href="/favorite">Favorite</a></li>
-                    <li><a href="/about">About</a></li>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined}
+                    to="/">Home</NavLink></li>
+                    <li><NavLink className={({isActive}) => isActive ? styles.linkActive : undefined}
+                    to="/favorite">Favorite</NavLink></li>
+                    <li><NavLink className={({isActive}) => isActive ? styles.linkActive : undefined}
+                    to="/about">About</NavLink></li>
                 </ul>
             </div>
         </nav>
