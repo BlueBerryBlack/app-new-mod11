8528f7c230b6c6b3aa9da24b6d45a50ba4fe7c30
Jagoda Pogroszewsla <pogroszewskadesign@gmail.com>
2023-10-25 10:47:53 +0200

    link-List

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import Home from './components/Home/Home'
 import Container from './components/Container/Container';
 import About from './components/About/About';
 import Favorite from './components/Favorite/Favorite';
-import { Routes, Route } from 'react-router-dom';
+import List from './components/List/List'
 import PageNotFound from './components/PageNotFound/PageNotFound';
+
+import { Routes, Route } from 'react-router-dom';
+
 
 const App = () => {
   return (
@@ -15,6 +18,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path = "/About" element={<About/>} />
           <Route path = "/Favorite" element={<Favorite />} />
+          <Route path = "/List" element={<List />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Container>
